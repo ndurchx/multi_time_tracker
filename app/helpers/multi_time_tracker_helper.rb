07d054ff233f49f426eb45f516429757701b09c3
Kevin Michael Neuenfeldt <kevin@kevin-laptop.site>
2013-03-29 16:01:28 +0100

    Removed unnessecary formatted_active_time function

--- app/helpers/multi_time_tracker_helper.rb
+++ app/helpers/multi_time_tracker_helper.rb
@@ -1,18 +1,17 @@
 module MultiTimeTrackerHelper
-
-   def formatted_active_time(logging)
-    seconds_at_all = logging.spent_seconds + (Time.now - logging.activated_at)
-    t = Time.new(0) + seconds_at_all
-    return (t.yday > 1 ? "#{t.yday}:#{t.strftime("%H:%M:%S")}" : t.strftime("%H:%M:%S"))
-  end
   
-  def formatted_time(logging)
-    unless logging.activated_at.nil?
-      t = Time.new(0) + logging.spent_seconds
+  def formatted_time(logged_time)
+    if logged_time.active
+      seconds_at_all = logged_time.spent_seconds + (Time.now - logged_time.activated_at)
+      t = Time.new(0) + seconds_at_all
       return (t.yday > 1 ? "#{t.yday}:#{t.strftime("%H:%M:%S")}" : t.strftime("%H:%M:%S"))
+    else
+      unless logged_time.activated_at.nil?
+        t = Time.new(0) + logged_time.spent_seconds
+        return (t.yday > 1 ? "#{t.yday}:#{t.strftime("%H:%M:%S")}" : t.strftime("%H:%M:%S"))
+      end
+      return "00:00:00"
     end
-    
-    return "00:00:00"
   end
   
 end
