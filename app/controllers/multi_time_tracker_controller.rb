53370a5dace56c8349f51b27584c984c712de3b4
Kevin Neuenfeldt <kevin.neuenfeldt@googlemail.com>
2013-10-08 15:57:24 +0200

    Added helper :timelog because it's neccessary since nwe Rails version

--- app/controllers/multi_time_tracker_controller.rb
+++ app/controllers/multi_time_tracker_controller.rb
@@ -6,6 +6,8 @@ class MultiTimeTrackerController < ApplicationController
   before_filter :find_project, :only => :add
   before_filter :is_time_tracking_active?, :only => :add
 
+  helper :timelog
+
   def index
     @tracked_times = LoggedTime.find_all_by_user_id(User.current.id)
     @tracked_times.sort!{|x,y| x.project_id <=> y.project_id}
@@ -21,7 +23,7 @@ class MultiTimeTrackerController < ApplicationController
     @logged_time.active = false
     @logged_time.activated_at = nil
     @logged_time.spent_seconds = 0
-    
+
     respond_to do |format|
       if @logged_time.save
         flash[:notice] = l(:multi_time_tracker_tracking_successfully_created)
@@ -117,22 +119,22 @@ class MultiTimeTrackerController < ApplicationController
       else
         flash[:notice] = l(:multi_time_tracker_export_all_successful)
       end
-      
+
       format.html { redirect_to :action => :index }
     end
   end
-  
-  def export 
+
+  def export
     check_out_logging(@logged_time) if @logged_time.active
-    
+
     respond_to do |format|
-      if export_to_timelog(@logged_time) 
+      if export_to_timelog(@logged_time)
         reset(@logged_time)
         flash[:notice] = l(:multi_time_tracker_export_successful)
       else
         flash[:error] = l(:multi_time_tracker_export_unsuccessful)
       end
-      
+
       format.html { redirect_to :action => :index }
     end
   end
@@ -142,54 +144,54 @@ class MultiTimeTrackerController < ApplicationController
 
 
   private
-  
+
   def find_project
     @issue = Issue.find_by_id(params[:issue_id])
     @project = Project.find_by_id(@issue.project_id)
     rescue ActiveRecord::RecordNotFound
     render_404
   end
-  
+
   def find_logged_time
     @logged_time = LoggedTime.find_by_id(params[:id])
   end
-  
+
   def user_logged_in
     unless User.current.logged?
       flash[:error] = l(:multi_time_tracker_user_not_logged_in)
       redirect_to :home
     end
   end
-  
+
   def check_out_logging(logged_time)
     logged_time.active = false
     logged_time.spent_seconds += (Time.now.to_f - logged_time.activated_at.to_f).to_i
   end
-  
+
   def export_to_timelog(logged_time)
     return false if logged_time.project.module_enabled?(:time_tracking).nil?
     spent_hours = logged_time.spent_seconds/60.0/60.0
-    
-    if spent_hours > 0.008 
+
+    if spent_hours > 0.008
       time_entry = TimeEntry.new(:project => logged_time.project, :issue => logged_time.issue, :user => logged_time.user, :spent_on => User.current.today)
       time_entry.safe_attributes = { "spent_on" => User.current.today, "hours" => spent_hours, "activity_id" => logged_time.activity_id, "comments" => logged_time.comment }
       return time_entry.save
     end
-    
+
     return true
   end
-  
+
   def reset(logged_time)
     logged_time.spent_seconds = 0
     logged_time.comment = ""
     logged_time.save
   end
-  
+
   def is_time_tracking_active?
     if (@project.module_enabled? :time_tracking).nil?
       flash[:error] = l(:multi_time_tracker_time_tracking_inactive)
       redirect_to request.referer
     end
   end
-  
+
 end
