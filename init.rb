6fd6c1669b4f16b0917c7607d1ba9a5a7ec71531
Kevin Neuenfeldt <kevin.neuenfeldt@googlemail.com>
2014-01-15 12:52:37 +0100

    Rearranged table headers of time tracker table
    The headers of the time tracker table were one element to far 
    on the right. I've put them all together one element to the left so
    they fit the columns they describe.
    
    Additionally I set the right URL for github repo in init.rb.

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :multi_time_tracker do
   will be able to add entries for issues to the Multi Time Tracker by clicking a link in issues view. If the user is allowed to make 
   entries to time log, he will even be able to export his tracked times to it. And don\'t be scared, all actions are fail save.'
   version '0.1.0'
-  url 'http://www.enervision.eu'
+  url 'https://github.com/raafael911/issue_repo_history_merge'
   author_url ''
 
 	requires_redmine :version_or_higher => '2.0.0'
