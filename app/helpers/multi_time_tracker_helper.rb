e29808caaee405188634e19acbaba5753cae20e5
ndurchx <christoph.schuster@n-durch-x.de>
2014-04-23 13:39:12 +0200

    Support Ruby 1.8
    Time.now() does work different in Ruby 1.8

--- app/helpers/multi_time_tracker_helper.rb
+++ app/helpers/multi_time_tracker_helper.rb
@@ -3,11 +3,11 @@ module MultiTimeTrackerHelper
   def formatted_time(logged_time)
     if logged_time.active
       seconds_at_all = (logged_time.spent_hours*3600 + (Time.now - logged_time.activated_at))
-      t = Time.new(0) + seconds_at_all
+      t = Time.local(0) + seconds_at_all
       return (t.yday > 1 ? "#{t.yday}:#{t.strftime("%H:%M:%S")}" : t.strftime("%H:%M:%S"))
     else
       unless logged_time.activated_at.nil?
-        t = Time.new(0) + logged_time.spent_hours*3600
+        t = Time.local(0) + logged_time.spent_hours*3600
         return (t.yday > 1 ? "#{t.yday}:#{t.strftime("%H:%M:%S")}" : t.strftime("%H:%M:%S"))
       end
       return "00:00:00"
