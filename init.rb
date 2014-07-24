5dca73c37521071c1cc6791d24d65f8950250643
Kevin Neuenfeldt <kevin.neuenfeldt@googlemail.com>
2014-07-24 20:50:26 +0200

    Bug with moved issues solved
    If an issue was moved from one project to another the reference to that project was not updated in the corresponding TrackedTime objects. But now this information is updated on each update on an issue.

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require_dependency 'multi_time_tracker/hooks'
 
-ActionDispatch::Callbacks.to_prepare  do
+ActionDispatch::Callbacks.to_prepare do
   Issue.send(:include, MultiTimeTracker::Patches::IssuePatch)
 end
 
@@ -10,8 +10,8 @@ Redmine::Plugin.register :multi_time_tracker do
   description 'This plugin adds a new time related functionality to your redmine. Logged in users with appropriate permission
   will be able to add entries for issues to the Multi Time Tracker by clicking a link in issues view. If the user is allowed to make 
   entries to time log, he will even be able to export his tracked times to it. And don\'t be scared, all actions are fail save.'
-  version '0.1.4'
-  url 'https://github.com/raafael911/issue_repo_history_merge'
+  version '0.1.5'
+  url 'https://github.com/raafael911/multi_time_tracker'
   author_url ''
 
 	requires_redmine :version_or_higher => '2.0.0'
--- lib/multi_time_tracker/patches/issue_patch.rb
+++ lib/multi_time_tracker/patches/issue_patch.rb
@@ -3,23 +3,12 @@ module MultiTimeTracker
     module IssuePatch
 
       def self.included(base)
-        base.extend(ClassMethods)
         base.send(:include, InstanceMethods)
         base.class_eval do
           unloadable
           after_destroy :remove_tracked_times_for_issue
+      	  after_update :update_tracked_times_for_issue
         end
-      end
-
-      module ClassMethods
-
-        def remove_tracked_times_for_issue
-          tracked_times = LoggedTime.find_all_by_user_id_and_issue_id(User.current.id, self.id)
-          tracked_times.each do |time|
-            time.destroy
-          end
-        end
-
       end
 
       module InstanceMethods
@@ -30,6 +19,14 @@ module MultiTimeTracker
             time.destroy
           end
         end
+
+        def update_tracked_times_for_issue
+          tracked_times = LoggedTime.find_all_by_user_id_and_issue_id(User.current.id, self.id)
+          tracked_times.each do |time|
+            time.project_id = self.project.id
+            time.save
+          end
+        end        
 
       end
 
